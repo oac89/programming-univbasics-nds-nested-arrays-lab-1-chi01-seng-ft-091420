53712a7b647d7647f181d604d0b635e7a21c2f93
Oscar A Castro <oacastro2@gmail.com>
2020-09-09 17:25:32 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,7 +49,7 @@ def assembled_matrix
 
  def matrix_lookup(matrix, row, column)
    matrix[row][column]
-#   # Given any matrix (array of arrays), a row index and a column index, 
+#   Given any matrix (array of arrays), a row index and a column index, 
 #   # Return the matrix's content at that row and and column
   p matrix[row][column]
   
